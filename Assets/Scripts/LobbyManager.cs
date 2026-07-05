de25f67858ee0b9e2cd8139fa8a2c5757b25f270
Srinu <srinu18yt@gmail.com>
2026-07-05 07:27:32 +0530

    Completed basic lobby manager to manage lobbies, need to create UI for Lobby

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    private Lobby hostLobby;
+    private Lobby currentLobby;
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "Lobby";
+            int maxPlayers = 2;
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            hostLobby = lobby;
+            currentLobby = lobby;
+
+            StartHeartBeat();
+            StartPollingLobbyData();
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions options = new QueryLobbiesOptions
+            {
+                Count = 15,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(true, QueryOrder.FieldOptions.Created)
+                }
+
+            };
+
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    private async void JoinLobbyByCode(string code)
+    {
+        try
+        {
+            Lobby lobby = await LobbyService.Instance.JoinLobbyByCodeAsync(code);
+            currentLobby = lobby;
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void QuickJoinLobby()
+    {
+        try
+        {
+            Lobby lobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+            currentLobby = lobby;
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    
+    public async void LeaveLobby()
+    {
+        try
+        {
+            await LobbyService.Instance.RemovePlayerAsync(currentLobby.Id, AuthenticationService.Instance.PlayerId);
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void StartHeartBeat()
+    {
+        try
+        {
+            if(hostLobby != null)
+            {
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+                await Task.Delay(15000);
+            }
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private async void StartPollingLobbyData()
+    {
+        try
+        {
+            Lobby lobby = await LobbyService.Instance.GetLobbyAsync(currentLobby.Id);
+            currentLobby = lobby;
+            await Task.Delay(3000);
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
