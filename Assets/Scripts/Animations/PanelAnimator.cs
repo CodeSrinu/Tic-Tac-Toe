d58b938c07b11c187940970e1e8f37f9315fc949
Srinu <srinu18yt@gmail.com>
2026-07-11 12:26:01 +0530

    downloaded UI elements

---
+++ Assets/Scripts/Animations/PanelAnimator.cs
@@ -0,0 +1,43 @@
+using DG.Tweening;
+using UnityEngine;
+
+public static class PanelAnimator
+{
+    public static float moveYDuration = 0.4f;
+    public static float rotateBackYDuration = 0.2f;
+    public static void Show(GameObject go)
+    {
+        if (go.activeSelf) return;
+
+        go.SetActive(true);
+        go.transform.DOKill();
+        go.transform.localPosition = new Vector3(0, 1550, 0);
+        go.transform.localRotation = Quaternion.identity;
+
+        Sequence sequence = DOTween.Sequence();
+
+        sequence
+            .Append(go.transform.DOLocalMoveY(0f, moveYDuration))
+            .Join(go.transform.DOLocalRotate(new Vector3(0, 0, 12f), moveYDuration)).SetDelay(0.3f)
+            .Append(go.transform.DOLocalRotate(Vector3.zero, rotateBackYDuration).SetEase(Ease.OutBack));
+        
+    }
+
+    public static void Hide(GameObject go)
+    {
+        go.transform.DOKill();
+
+        Sequence sequence = DOTween.Sequence();
+
+        sequence
+            .Append(go.transform.DOLocalRotate(new Vector3(0, 0, 12f), moveYDuration))
+            .Join(go.transform.DOLocalMoveY(1550f, moveYDuration).SetEase(Ease.InBack))
+
+            .OnComplete(() =>
+            {
+                go.SetActive(false);
+                go.transform.localPosition = Vector3.zero;
+            });
+    }
+
+}
---
+++ Assets/Scripts/Managers/BoostrapManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class BoostrapManager : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private LoadingScreenManager loadingScreenManager;
+    public static BoostrapManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        settingsPanel.SetActive(false);
+        loadingScreenManager.Hide();
+    }
+
+    public void ShowSettings() => PanelAnimator.Show(settingsPanel);
+    public void ShowLoading() => loadingScreenManager.Show();
+    public void HideLoading() => loadingScreenManager.Hide();
+
+
+}
